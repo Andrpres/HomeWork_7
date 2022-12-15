d1250acc8d456c4a1dd4bf521e903f41632f35bf
Andrpre <andrpre96@gmail.com>
2022-12-15 08:30:35 +0300

    Finish 2 task

---
+++ Task2/Program.cs
@@ -0,0 +1,73 @@
+﻿/*
+Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет
+*/
+
+//задать матрицу и заполнить ее числами
+int[,] GetMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+
+    return matrix;
+}
+
+//распечатать матрицу
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+//получить числа с консоли
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+
+    return result;
+}
+
+// Поиск значения заданного элемента
+void FindElement(int number, int[,] matrix){
+    int stroke = (number / 5);
+    int colon = (number % 5);
+
+    if(stroke > 4 || colon > 4) Console.WriteLine("такого числа в массиве нет");
+    else Console.WriteLine($"{matrix[stroke, colon]}");
+}
+
+int[,] matrix = GetMatrix(5, 5);
+PrintMatrix(matrix);
+
+FindElement(GetNumber("Введите число"), matrix);
